8595ce01404841a1864911fd73f54fa9264a043a
Sven <svenebal@gmail.com>
2020-01-07 00:18:11 -0600

    Detalles de cruck.php

--- Galadrassil/inc/cruck.php
+++ Galadrassil/inc/cruck.php
@@ -19,7 +19,7 @@
               $arbolInfo = $database->select('arboles',['tipo','etapa', 'adulto'],['id'=>$arbolID]);
               $arbolDetalles = $database->select('cuidados_arbol',['tiempo_agua','tiempo_abono','agua','abono'],['id_arbol'=>$arbolID]);
               echo '<li class="garden_pots_item">';
-              echo '<img onclick="mostrarPlanta(id,src)" data-id_unico="'.$arbolID.'" data-cuidados_abono="'.$arbolDetalles[0]['cuidados_abono'].'" data-cuidados_agua="'.$arbolDetalles[0]['cuidados_agua'].'" data-tiempo_actual="'.time().'" data-tiempo_abono="'.$arbolDetalles[0]['tiempo_abono'].'" data-tiempo_agua="'.$arbolDetalles[0]['tiempo_agua'].'" data-name="'.$arbolInfo[0]['tipo'].'" id="'.$contador.'" class="garden_pots_item_svg" src="./assets/svg/pot_'.$arbolInfo[0]['etapa'].'_'.$arbolInfo[0]['tipo'].'.svg" alt="Maceta">';
+              echo '<img onclick="mostrarPlanta(id,src)" data-id_arbol="'.$arbolID.'" data-abono="'.$arbolDetalles[0]['abono'].'" data-agua="'.$arbolDetalles[0]['agua'].'" data-tiempo_actual="'.time().'" data-tiempo_abono="'.$arbolDetalles[0]['tiempo_abono'].'" data-tiempo_agua="'.$arbolDetalles[0]['tiempo_agua'].'" data-name="'.$arbolInfo[0]['tipo'].'" id="'.$contador.'" class="garden_pots_item_svg" src="./assets/svg/t_'.$arbolInfo[0]['tipo'].'_'.$arbolInfo[0]['etapa'].'.png" alt="Maceta">';
               echo '</li>';
             }else{
               echo '<li class="garden_pots_item"><img onclick="mostrarPlanta(id,src);seleccionarTipo(1)" data-name="" id="'.$contador.'" class="garden_pots_item_svg" src="./assets/svg/pot_0_BoxPot.svg" alt="Maceta"></li>';
